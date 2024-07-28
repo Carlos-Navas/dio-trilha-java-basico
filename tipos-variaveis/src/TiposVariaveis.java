c2e2db88a80e733660bbc9aec4b6a22c6b67b551
carlos <carlosnqf@gmail.com>
2024-07-28 19:14:59 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,31 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+    //tipos primitivos
+    // (int,byte,short,long,float,douible,boolean,char)
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+    // int numero = 1;
+    // numero = 2;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        
+    // valor nunca muda (final)    
+
+        final double VALOR_DE_PI = 3.14;
+
+
+
+
+
+
+
     }
 }
